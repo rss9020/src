7f2cfc4e49a5a38fb370f070d635855c2b029ff1
rss9020 <59556293+rss9020@users.noreply.github.com>
2023-01-13 21:52:01 +0700

    first use array.map
    array of components its great

--- Components/Flowbar/Flowbar.jsx
+++ Components/Flowbar/Flowbar.jsx
@@ -1,13 +1,30 @@
 import Post from "./Post/Post";
 import FlModule from './Flowbar.module.css'
 
+let Posts = [
+    {name: 'Ivan', id: 0},
+    {name: 'Maria', id: 1}
+];
+
+
+
+let a = [
+    {name: 'Kerjik', like: 3},
+    {name: 'Sturjik', like: 5},
+    {name: 'Fufik', like: 6}
+    ];
+let b = a.map( (ddd) => <Post name={ddd.name} like={ddd.like}/>  );
+
 function Flowbar() {
     return (
 
         <div><h3>Lenta</h3>
-            <Post name='Ivan' like='8'/>
+            <Post name='Ivan' like='0'/>
             <Post name='Maria' like='6'/>
-
+           {
+               [<Post name='Sidor' like='3'/>, <Post name='Serik' like='2'/>]
+           }
+            {b}
 
         </div>
 
